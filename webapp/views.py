5e6bfc57bcd7be8c64029794f3de898557a2560b
Etile9998 <gosehwan@gmail.com>
2023-01-25 10:37:21 +0900

    calcTime 제거, 개선 필요

---
+++ webapp/views.py
@@ -0,0 +1,44 @@
+from django.contrib import auth
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .models import Info
+from django.views.generic import ListView
+
+
+# Create your views here.
+
+class PostList(ListView):
+    model = Info
+    ordering = '-pk'
+    paginate_by = 1
+
+
+def createform(request):
+    std = Info()
+    std.battery = request.GET['battery']
+    std.color = request.GET['color']
+    std.runtime = request.GET['runtime']
+    std.save()
+
+    return redirect('/#about')
+
+
+def signin(request):
+    if request.method == "GET":
+        return redirect('webapp:index')
+
+    elif request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('webapp:index')
+        else:
+            return redirect('webapp:index')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+    return redirect('webapp:index')
