eebe0101bd21b7633134aefcdb32ecf10f7d49fb
Etile9998 <gosehwan@gmail.com>
2023-01-31 07:07:35 +0900

    색상 표기 변환, runtime 표기 변환, TODO: 날짜 표기 한글로

--- webapp/views.py
+++ webapp/views.py
@@ -6,15 +6,13 @@ from django.views.generic import ListView
 
 # Create your views here.
 
+
 class PostList(ListView):
    model = Info
    ordering = '-pk'
 
 
-def calc_time(ms):
-    if ms == 0:
-        return 'NULL'
-
+def get_runtime(ms):
     second = int(int(ms)/1000)
     minute = int(second/60)
     second %= 60
@@ -28,17 +26,38 @@ def calc_time(ms):
         result += str(minute) + '분 '
     if second > 0:
         result += str(second) + '초 '
+
+    if hour == 0 and minute == 0 and second == 0:
+        result = '0초'
     return result
+
+
+def get_date(eng):
+    eng_split = eng.split(' ')
+    month_table = {
+        'Jan.': '1월',
+        'Feb.': '2월',
+        'Mar.': '3월',
+        'Apr.': '4월',
+        'May.': '5월',
+        'Jun.': '6월',
+        'Jul.': '7월',
+        'Aug.': '8월',
+        'Sep.': '9월',
+        'Oct.': '10월',
+        'Nov.': '11월',
+        'Dec.': '12월',
+    }
+    return f'{eng_split[2]}년 {month_table[eng_split[0]]}월 {eng_split[1]}일 {eng_split[3]} {eng_split[4]}'
 
 def createform(request):
    std = Info()
    std.battery = request.GET['battery']
    std.color = request.GET['color']
-   std.runtime = request.GET['runtime']
+   std.runtime = get_runtime(request.GET['runtime'])
    std.firmV = request.GET['firmV']
-
+   std.date = get_date(request.GET['created_at'])
    std.save()
-
    return redirect('/')
 
 
