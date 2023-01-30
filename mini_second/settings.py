6e66916da72f908d2ac4cd21a4d9d48a6d46ba7e
kkmh90 <118958101+kkmh90@users.noreply.github.com>
2023-01-30 18:00:06 +0900

    헤더이미지 캐러셀 변경(수정필요)

--- mini_second/settings.py
+++ mini_second/settings.py
@@ -128,7 +128,7 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
-STATICFILES_DIRS = [BASE_DIR/'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
